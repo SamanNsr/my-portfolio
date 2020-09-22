c18d55d17009a7fea7a19069acbad4526c753013
Saman2000hp <1999saman@gmail.com>
2020-09-23 01:39:13 +0330

    fix th screen h

--- components/MainBody/SimpleBody.js
+++ components/MainBody/SimpleBody.js
@@ -3,7 +3,7 @@ import React from 'react';
 function SimpleBody(props) {
   let className = 'bg-white mb-20  py-12';
   if (props.fullScreen === true) {
-    className = 'bg-white mt-20 h-screen';
+    className = 'bg-white mt-20 max-h-screen';
   }
   return <section className={className}>{props.children}</section>;
 }
