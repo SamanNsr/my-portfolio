5d28afa50a45a250cd37fdaa16c8d7be031b3348
Saman2000hp <1999saman@gmail.com>
2020-09-23 01:27:54 +0330

    fix animation speed

--- components/AnimatedTyping/AnimatedTyping.js
+++ components/AnimatedTyping/AnimatedTyping.js
@@ -4,6 +4,7 @@ import Typing from 'react-typing-animation';
 const AnimatedTypingComponent = () => (
   <Typing>
     <div className="text-xl text-gray-800 mt-4">
+      <Typing.Speed ms={100} />
       PV = Block
       <Typing.Backspace count={11} />
       <Typing.Speed ms={50} />
